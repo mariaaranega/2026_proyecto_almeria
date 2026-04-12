0552c61cc3863c9707289b0f6db4aa260943794f
violetagm <viogm100@gmail.com>
2026-04-12 18:12:13 +0200

    Hecha base de datos medidas piezómetro de 5 masas

--- 05_scripts/04_importar_datos_concretos_piezometros_igme.R
+++ 05_scripts/04_importar_datos_concretos_piezometros_igme.R
@@ -1,4 +1,6 @@
 # 05 leer datos concretos piezómetros (por masa de agua)
+# Necesario cambiar la ruta por cada masa de agua
+
 
 
 # Cargar librerías -------------------------------------------------------------
@@ -7,14 +9,16 @@ library(janitor)
 library(readxl)
 library(stringr)
 
+
+
 # Leer nombres archivos --------------------------------------------------------
 nombres_xlsx_ruta <- list.files(
-  path = "03_datos_brutos/datos_piezometros/campo_dalias_sierra_gador", 
+  path = "03_datos_brutos/datos_piezometros/oeste_sierra_gador", 
   pattern = ".xlsx",
   full.names = T) # Obtener ruta completa
 
 nombres_xlsx <- list.files(
-  path = "03_datos_brutos/datos_piezometros/campo_dalias_sierra_gador",
+  path = "03_datos_brutos/datos_piezometros/oeste_sierra_gador",
   pattern = ".xlsx",
   full.names = F) |> # Obtener nombre del archivo
   str_remove_all(pattern = ".xlsx") |> 
@@ -44,24 +48,24 @@ datos <-
            tipo_surgencia)) 
 
 
-# HASTA AQUÍ ESTÁ BIEN ----------------------------------------------------------
 
 # Filtrar los piezómetros que tengan más de una medida por año --------------------
-datos$fecha_2 <- as.Date(datos$fecha_2, format = "%d/%m/%y") # Establecer formato fecha
-datos |> 
+datos$fecha_2 <- as.Date(datos$fecha_2, format = "%d/%m/%Y") # Establecer formato fecha
+datos_filtrados <- datos |> # Descartar piezómetros con solo un año
+  mutate(anno = year(fecha_2)) |> 
   group_by(id_piezometro) |> 
-  summarise(fecha_2 = n()) |> 
-  filter(fecha_2 > 1) |> 
+  filter(n_distinct(anno) > 1) |> 
   ungroup()
-datos <- datos |> 
-  mutate(anno = year(fecha_2)) 
-
- 
+datos_medios <- datos_filtrados |> 
   group_by(id_piezometro, anno) |> 
-  summarise(cota_np_media = mean(nivel_piezometrico_m_s_n_m, na.rm=T)) |> 
-  print(n = 61)
+  summarise(
+    media_nivel = mean(nivel_piezometrico_m_s_n_m, na.rm=T),
+    n_medidas = n(),
+    sd_nivel = sd(nivel_piezometrico_m_s_n_m, na.rm=T)
+  ) |> 
+  ungroup()
 
   
   
 # Exportar archivo -------------------------------------------------------------
-write.csv(datos, "04_datos_procesados/piezometros/base_piezometros_igme.csv")
+write.csv(datos_medios, "04_datos_procesados/piezometros/base_piezometros_igme_oeste_sierra_gador.csv")
